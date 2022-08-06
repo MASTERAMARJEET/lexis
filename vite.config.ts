2274aac933ee7afdf58e012cae5648e6ccedf85e
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-08-06 21:18:56 +0530

    add vite tsconfig path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import Icons from 'unplugin-icons/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
+    tsConfigPaths(),
     Icons({
       autoInstall: true,
       compiler: 'svelte',
