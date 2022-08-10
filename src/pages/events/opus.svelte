5f58ddb4a03a01f5916935e69c8d9e195791f30c
Harshith <pystardust@protonmail.com>
2022-08-10 22:44:34 +0530

    feat: added more events

---
+++ src/pages/events/opus.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import H1 from 'src/md/H1.svelte'
+  import H2 from 'src/md/H2.svelte'
+  import P from 'src/md/P.svelte'
+  import Strong from 'src/md/Strong.svelte'
+  import List from 'src/md/List.svelte';
+</script>
+
+<H1>Opus</H1>
+<P>
+We are told not to judge a book by its cover. But let's admit it, they are the ones that grab our attention and tempt us to reach out towards the bookshelf. Whether covered with layers of references slowly peeled off as you pore through the pages, or giving you a feel of what is about to ensue, book cover rightly serves as the gateway to your promising journey. So why not try out this exciting art form for yourself? 
+</P>
+
+<H2>Rules</H2>
+<List>
+	<li>
+Participation is individual
+	</li>
+	<li>
+Drawing materials will be provided by us. Additional materials need to be discussed with the organiser before bringing.
+	</li>
+	<li>
+Participants will be given three choices for medium: watercolour, pencil sketch and pencil colour (will specify the details after procurement of materials)
+	</li>
+	<li>
+Participant will be able to choose a genre from the list and a book will be assigned to them on spot based on the genre.
+	</li>
+	<li>
+Synopsis and other relevant details will be provided. Plot summary or any additional info can also be given on demand. Participant can surf through net for other things or inspiration but similarity to any available covers of the book will lead to disqualification. And inspirations are very sneaky creatures, once you see the respective covers, you might not be able to think further, so beware! 
+	</li>
+</List>
+
+<P>
+  <Strong>Organiser:</Strong>
+  Surajit Manna
+</P>
+<P>
+  <Strong>Contact:</Strong>
+  +91 8617326310
+</P>
