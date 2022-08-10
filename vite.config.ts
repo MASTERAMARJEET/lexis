5dfc154d9e45af8601f8c816778f753d48dbc9d2
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-08-10 15:58:05 +0530

    add and setup mdsvex

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import Icons from 'unplugin-icons/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { mdsvex } from 'mdsvex'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    svelte(),
+    svelte({
+      // these are the defaults. If you want to add more extensions, see https://mdsvex.pngwn.io/docs#extensions
+      extensions: ['.svelte', '.svx', '.md'],
+      preprocess: mdsvex({
+        extensions: ['.svx', '.md'],
+      }),
+    }),
     tsConfigPaths(),
     Icons({
       autoInstall: true,
