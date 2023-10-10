a13c7b9ba1edee94f79adc9050f4e3b5d9d3917c
Sattwamo Ghosh <107874535+sattwamo@users.noreply.github.com>
2023-10-11 02:19:00 +0530

    added debate topics (#12)
    * corrected the dates
    
    * updated mad ads timing
    
    * added war of words debate topics

--- src/pages/events/debate.svelte
+++ src/pages/events/debate.svelte
@@ -12,6 +12,18 @@
   As an institute committed to the pursuit of truth, War of Words, Lexis' famed debate event, tests the mettle of ideas and the debaters defending them. With a plethora of spicy topics to choose from, this event will surely have you on the edge of your seats.
 </P>
 
+<P>The debate topics for this year's event are :- </P>
+<List>
+  <li>Scientism is the new world religion.</li>
+  <li>Therapy fuels narcissism.</li>
+  <li>Obesity is a choice.</li>
+  <li>All present methods to curb climate change are futile.</li>
+  <li>People should be free to sell themselves as slaves. </li>
+  <li>"Modern Art" has no artistic value and is a front for money laundering.</li>
+  <li>The United Nations and its subsidiary bodies are corrupt institutions and fail to serve their purpose.</li>
+  <li>Veganism is the ethical and practical dietary choice of the future.</li>
+</List>
+
 <P>
   <Strong>Organiser:</Strong>
   Jesneil Lauren Lewis
